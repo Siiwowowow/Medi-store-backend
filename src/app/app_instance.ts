b0a647b217a271b8eb8e461bc40e4d628af21fba
Siiwowowow <siaa32290@gmail.com>
2026-05-11 23:29:49 +0600

    solve backend link problem

--- src/app/app_instance.ts
+++ src/app/app_instance.ts
@@ -22,7 +22,7 @@ app.use("/api/v1/payment/webhook", express.raw({ type: "application/json" }));
 
 // Enable CORS with credentials
 app.use(cors({
-    origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
+    origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "https://medistore-fontend.vercel.app", "https://medi-store-backend-rho.vercel.app"],
     credentials : true,
     methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders : ["Content-Type", "Authorization"]
